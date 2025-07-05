b0a7affcd8531f8bbafe8d8d9e40dd9bb6e4a708
Carlo Valdivia Luna <cvaldivialu@unsa.edu.pe>
2025-07-05 00:08:32 -0500

    feat: integrate NATS microservice with environment variable validation and configuration

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,29 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { envs } from './config/envs';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(envs.port);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.NATS,
+      options: {
+        servers: envs.natsServers,
+      },
+    },
+  );
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
+  console.log('election-Testing log');
+
+  await app.listen();
 }
 bootstrap();
