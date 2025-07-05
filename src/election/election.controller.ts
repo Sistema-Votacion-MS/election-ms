d0fd9042b7b177a8bff0c814fc48472eab156ddf
Carlo Valdivia Luna <cvaldivialu@unsa.edu.pe>
2025-07-05 00:08:15 -0500

    feat: enhance election service with pagination and error handling for CRUD operations

--- src/election/election.controller.ts
+++ src/election/election.controller.ts
@@ -3,33 +3,34 @@ import { MessagePattern, Payload } from '@nestjs/microservices';
 import { ElectionService } from './election.service';
 import { CreateElectionDto } from './dto/create-election.dto';
 import { UpdateElectionDto } from './dto/update-election.dto';
+import { ElectionPaginationDto } from './dto/election-pagination.dto';
 
 @Controller()
 export class ElectionController {
-  constructor(private readonly electionService: ElectionService) {}
+  constructor(private readonly electionService: ElectionService) { }
 
-  @MessagePattern('createElection')
+  @MessagePattern({ cmd: 'election_create' })
   create(@Payload() createElectionDto: CreateElectionDto) {
     return this.electionService.create(createElectionDto);
   }
 
-  @MessagePattern('findAllElection')
-  findAll() {
-    return this.electionService.findAll();
+  @MessagePattern({ cmd: 'election_find_all' })
+  findAll(@Payload() electionPaginationDto: ElectionPaginationDto) {
+    return this.electionService.findAll(electionPaginationDto);
   }
 
-  @MessagePattern('findOneElection')
-  findOne(@Payload() id: number) {
+  @MessagePattern({ cmd: 'election_find_one' })
+  findOne(@Payload() id: string) {
     return this.electionService.findOne(id);
   }
 
-  @MessagePattern('updateElection')
+  @MessagePattern({ cmd: 'election_update' })
   update(@Payload() updateElectionDto: UpdateElectionDto) {
     return this.electionService.update(updateElectionDto.id, updateElectionDto);
   }
 
-  @MessagePattern('removeElection')
-  remove(@Payload() id: number) {
+  @MessagePattern({ cmd: 'election_delete' })
+  remove(@Payload() id: string) {
     return this.electionService.remove(id);
   }
 }
--- src/election/election.service.ts
+++ src/election/election.service.ts
@@ -1,26 +1,103 @@
-import { Injectable } from '@nestjs/common';
+import { HttpStatus, Injectable, Logger, OnModuleInit } from '@nestjs/common';
 import { CreateElectionDto } from './dto/create-election.dto';
 import { UpdateElectionDto } from './dto/update-election.dto';
+import { PrismaClient } from 'generated/prisma';
+import { ElectionPaginationDto } from './dto/election-pagination.dto';
+import { RpcException } from '@nestjs/microservices';
 
 @Injectable()
-export class ElectionService {
+export class ElectionService extends PrismaClient implements OnModuleInit {
+
+  private readonly logger = new Logger('ElectionService');
+
+  async onModuleInit() {
+    await this.$connect();
+    this.logger.log('Database connected');
+  }
+
   create(createElectionDto: CreateElectionDto) {
-    return 'This action adds a new election';
+    try {
+      return this.election.create({
+        data: createElectionDto
+      });
+    } catch (error) {
+      throw new RpcException({
+        status: HttpStatus.BAD_REQUEST,
+        message: 'Check logs',
+      });
+    }
   }
 
-  findAll() {
-    return `This action returns all election`;
+  async findAll(electionPaginationDto: ElectionPaginationDto) {
+    const { status, page, limit } = electionPaginationDto;
+
+    const total = await this.election.count({ where: { status: status, is_active: true } });
+    const lastPage = Math.ceil(total / limit);
+
+    return {
+      data: await this.election.findMany({
+        skip: (page - 1) * limit,
+        take: limit,
+        where: {
+          status: status,
+          is_active: true,
+        },
+      }),
+      meta: {
+        total: total,
+        page: page,
+        lastPage: lastPage,
+      },
+    };
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} election`;
+  async findOne(id: string) {
+    const election = await this.election.findUnique({
+      where: { id: id, is_active: true },
+    });
+
+    if (!election) {
+      throw new RpcException({
+        status: HttpStatus.NOT_FOUND,
+        message: `Election with id ${id} not found`,
+      });
+    }
+
+    return election;
   }
 
-  update(id: number, updateElectionDto: UpdateElectionDto) {
-    return `This action updates a #${id} election`;
+  async update(id: string, updateElectionDto: UpdateElectionDto) {
+    try {
+      const { id: __, ...data } = updateElectionDto;
+
+      await this.findOne(id);
+
+      return this.election.update({
+        where: { id: id },
+        data: data,
+      });
+    } catch (error) {
+      throw new RpcException({
+        status: HttpStatus.BAD_REQUEST,
+        message: 'Check logs',
+      });
+    }
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} election`;
+  async remove(id: string) {
+    try {
+      await this.findOne(id);
+
+      return this.election.update({
+        where: { id: id },
+        data: { is_active: false },
+      });
+    } catch (error) {
+      throw new RpcException({
+        status: HttpStatus.BAD_REQUEST,
+        message: 'Check logs',
+      });
+    }
+
   }
 }
