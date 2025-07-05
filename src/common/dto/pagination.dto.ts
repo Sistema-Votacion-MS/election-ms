cbae3178f3e1521f7a80317897e952a659874e7d
Carlo Valdivia Luna <cvaldivialu@unsa.edu.pe>
2025-07-05 00:07:40 -0500

    fix: change pagination properties to required fields in PaginationDto

--- src/common/dto/pagination.dto.ts
+++ src/common/dto/pagination.dto.ts
@@ -6,11 +6,11 @@ export class PaginationDto {
   @IsPositive()
   @IsOptional()
   @Type(() => Number)
-  page?: number = 1;
+  page: number = 1;
 
   @IsPositive()
   @IsOptional()
   @Type(() => Number)
-  limit?: number = 10;
+  limit: number = 10;
 
 }
